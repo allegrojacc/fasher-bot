70a39cc390e8ddcf28c532abbd076fe0c085cef4
allegrojacc <allegrojacc@gmail.com>
2026-04-23 12:37:34 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import re
 import discord
 from discord.ext import commands
 
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True  # potrzebne do czytania treści wiadomości
